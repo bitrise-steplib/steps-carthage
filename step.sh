ff4d8999a716715e924e815dbe6cec637851f08f
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-08-06 15:19:55 +0200

    Add project dir path input param

--- step.sh
+++ step.sh
@@ -3,10 +3,21 @@
 set -e
 
 #
+# Required parameters
+if [ -z "${project_dir}" ] ; then
+	echo "[!] Missing required input: project_dir"
+	exit 1
+fi
+
+#
 # Make sure brew has Carthage installed
 brew update
 brew install carthage
 
+pushd "${project_dir}"
+
 #
 # Bootstrap
 carthage bootstrap --platform iOS
+
+popd
