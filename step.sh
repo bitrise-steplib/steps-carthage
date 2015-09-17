74f6e4e8e5400c483867e70d8208ae2905d8f1d7
Agnes Vasarhelyi <vasarhelyi.agi@gmail.com>
2015-09-17 11:21:59 +0200

    No need for manually require input param

--- step.sh
+++ step.sh
@@ -3,13 +3,6 @@
 set -e
 
 #
-# Required parameters
-if [ -z "${project_dir}" ] ; then
-	echo "[!] Missing required input: project_dir"
-	exit 1
-fi
-
-#
 # Make sure brew has Carthage installed
 brew update
 brew install carthage
