f19e7efe1d66b6614704a064107654e6f75fc7c3
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-10-27 15:42:47 +0100

    Add input to use brew default carthage or install 0.9.4 from package

--- step.sh
+++ step.sh
@@ -5,6 +5,14 @@ set -e
 NO_USE_BINARIES=""
 VERBOSE_MODE=""
 
+if [[ "${carthage_version}" == "0.9.4" ]] ; then
+	curl -OlL "https://github.com/Carthage/Carthage/releases/download/0.9.4/Carthage.pkg"
+	sudo installer -pkg "Carthage.pkg" -target /
+	rm "Carthage.pkg"
+else
+	brew update && brew install carthage
+fi
+
 if [[ "${no_use_binaries}" == "true" ]] ; then
 	NO_USE_BINARIES='--no-use-binaries'
 fi
