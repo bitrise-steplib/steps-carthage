a3902641f573dcf72767c91a4576ef0364ffa24b
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-10-27 12:51:34 +0100

    Set optional verbose mode to carthage command

--- step.sh
+++ step.sh
@@ -3,11 +3,16 @@
 set -e
 
 NO_USE_BINARIES=""
+VERBOSE_MODE=""
 
 if [[ "${no_use_binaries}" == "true" ]] ; then
 	NO_USE_BINARIES='--no-use-binaries'
 fi
 
+if [[ "${verbose_output}" == "true" ]] ; then
+	VERBOSE_MODE='--verbose'
+fi
+
 #
 # Bootstrap
-carthage "${carthage_command}" --platform iOS ${NO_USE_BINARIES}
+carthage "${carthage_command}" --platform iOS ${NO_USE_BINARIES} ${VERBOSE_MODE}
