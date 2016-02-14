3839bced46c7dc94ebade16447f929b83e58f4ef
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-02-14 20:06:54 +0100

    Add swift step script

---
+++ step.swift
@@ -0,0 +1,54 @@
+#!/usr/bin/swift
+
+import Foundation
+
+typealias ArgsArray = Array<String>
+
+func collectArgs(env: [String : String]) -> ArgsArray {
+    var args = ArgsArray()
+
+    if let platform = env["platform"] {
+        args.append("platform " + platform)
+    }
+
+    if let verboseOutput = env["verbose_output"] where verboseOutput == "true" {
+        args.append("verbose")
+    }
+
+    if let noUseBinaries = env["no_use_binaries"] where noUseBinaries == "true" {
+        args.append("no-use-binaries")
+    }
+
+    if let sshOutput = env["ssh_output"] where sshOutput == "true" {
+        args.append("use-ssh")
+    }
+
+    if let carthageOptions = env["carthage_options"] {
+        args.append(carthageOptions)
+    }
+
+    return args
+}
+
+let env = NSProcessInfo.processInfo().environment
+let task = NSTask()
+
+if let workingDir = env["working_dir"] as String! {
+    task.currentDirectoryPath = workingDir
+}
+
+if let carthageCommand = env["carthage_command"] {
+  let command = "carthage \(carthageCommand)"
+  let args = " " + ( collectArgs(env).map { "--\($0)" } ).joinWithSeparator(" ")
+
+  task.launchPath = "/bin/bash"
+  task.arguments = ["-c", command + args]
+
+  // run the shell command
+  task.launch()
+
+  // ensure to be finished before another command can run
+  task.waitUntilExit()
+} else {
+  fatalError("no command to execute")
+}
